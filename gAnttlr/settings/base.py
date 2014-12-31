a5dada688864024923594c6961fe2f305a57efe2
Craig <pureferret@gmail.com>
2014-12-31 15:20:16 +0000

    Current config only points at the main project not the app, so moving .git file and comitting

--- gAnttlr/settings/base.py
+++ gAnttlr/settings/base.py
@@ -29,14 +29,20 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+PREREQ_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-)
+]
+
+PROJECT_APPS = [
+    'gantt_charts',
+]
+
+INSTALLED_APPS = PREREQ_APPS + PROJECT_APPS
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
